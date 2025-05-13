f07b3a6ee7bb50ca33f86d30e268a73479520831
vika-asco-qa <129049786+vika-asco-qa@users.noreply.github.com>
2025-05-13 17:52:11 +0400

    Update Main.java

--- src/main/java/asco/vika/Main.java
+++ src/main/java/asco/vika/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and Asco.Vika!!!");}//Добавила знаки
+        System.out.printf("Hello and Asco.Vika!!!!");}//Добавила знаки
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
